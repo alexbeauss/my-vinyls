65bf510e1fb266fa2913042675bff641cbf3421c
AlexBeaussier <alex.beaussier@gmail.com>
2024-11-12 11:33:15 +0100

    width

--- app/home/ClientHome.js
+++ app/home/ClientHome.js
@@ -201,7 +201,7 @@ export default function ClientHome({ onAlbumClick }) {
               </div>
             ))}
           </div>
-          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-6">
+          <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-6 gap-6">
             {sortedAndFilteredReleases && sortedAndFilteredReleases.map((release) => (
               <div 
                 key={release.id} 
